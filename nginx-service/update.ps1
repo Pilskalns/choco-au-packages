302f73c2bfd7b780bc93e2d185520b01cd1dd044
Andzs Pilskalns <andzs@pilskalns.lv>
2019-01-19 01:02:12 +0200

    [skip ci]

--- nginx-service/update.ps1
+++ nginx-service/update.ps1
@@ -9,8 +9,8 @@
     Copy-Item "nginx-service.nuspec.$type" "nginx-service.nuspec" -Force | out-null
     
     try {
-        Update-Package -ChecksumFor none -NoCheckChocoVersion
-        # Update-Package -ChecksumFor none
+        # Update-Package -ChecksumFor none -NoCheckChocoVersion
+        Update-Package -ChecksumFor none
     } catch {
         $ignore = 'Unable to connect to the remote server'
         if ($_ -match $ignore) { Write-Host $ignore; 'ignore' }  else { throw $_ }
