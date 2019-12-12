85d86b5f0e2a1a24efce5a96f31054b2aa2e8723
Chocolatey <chocolatey@realdimensions.net>
2019-12-12 00:21:58 +0000

    AU: 1 updated - midi2lr [skip ci] https://gist.github.com/1b0f0584554f6e98b14aca7f2fec2230/1078ce2786fa7b78aa800bb96ddbc412dff2af17

--- midi2lr/tools/chocolateyinstall.ps1
+++ midi2lr/tools/chocolateyinstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   fileType      = 'EXE'
   softwareName  = 'midi2lr*'
 
-  url           = 'https://github.com/rsjaffe/MIDI2LR/releases/download/v3.4.2.1/MIDI2LR-3.4.2.1-windows-installer.exe'
-  checksum      = 'ea3660251153d0c82496514ae5112ee731d46c927443c667bde89d0b79267f88'
+  url           = 'https://github.com/rsjaffe/MIDI2LR/releases/download/v3.4.3.0/MIDI2LR-3.4.3.0-windows-installer.exe'
+  checksum      = '4548ec82a91ead87d84ecaeeafc56a15c5ee60b24f1b46a3058468909c15f96f'
   checksumType  = 'sha256'
  
   silentArgs    = '--mode unattended --unattendedmodeui none'
